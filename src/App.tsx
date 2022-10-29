cafc164b722398f148b0c2b39f1d11f5310818cb
Yvonne Hong <yvonnejhong@hotmail.com>
2022-10-28 17:38:42 -0700

    initial check in

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
+import { text } from 'stream/consumers';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App: FC = () => {
+  return <div className = "App">
+    <div className = "header">
+      <div className = "inputContainer">
+      <input type="text" placeholder = "My task:"/>
+      <input type="number" placeholder = "Time (in days):"/>
+      </div>
+      <button>Add task</button>
     </div>
-  );
+    <div className = "list"></div>
+  </div>
 }
 
 export default App;
